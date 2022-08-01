dcb0804e6515bbc9bb0c66227906c647a8ab67ae
onvito <lvictor462@gmail.com>
2022-08-01 15:05:52 -0300

    mentoria e curso

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,25 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("descrição curso java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso Js");
+		curso2.setDescricao("descrição curso js");
+		curso2.setCargaHoraria(7);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("mentoria de java");
+		mentoria.setDescricao("descrição mentoria java");
+		mentoria.setData(LocalDate.now());
+		
+		System.out.println(curso1);		System.out.println(curso2);
+		System.out.println(mentoria);
+	}
+}
